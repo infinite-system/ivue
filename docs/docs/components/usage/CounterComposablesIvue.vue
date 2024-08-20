a53ef40f5baef7f34a9d7a96adb7fe7f85a19e22
Evgeny <ekalashnikov@gmail.com>
2024-08-20 00:10:31 -0400

    add missing files

---
+++ docs/docs/components/usage/CounterComposablesIvue.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { CustomMouse } from './classes/CustomMouse';
+
+import { ivue, iref } from 'ivue';
+
+class Counter {
+  count = iref(0);
+
+  increment() {
+    this.count++;
+  }
+
+  mouse = ivue(CustomMouse, 5)
+}
+
+const counter = ivue(Counter);
+</script>
+<template>
+  <a href="javascript:void(0)" @click="() => counter.increment()">Increment</a>
+  Count: {{ counter.count }} <br />
+  Mouse X: {{ counter.mouse.x }}, Y: {{ counter.mouse.y }}
+  <br />
+  Total (computed): {{ counter.mouse.total }}
+  <br />
+  <button class="button" @click="() => counter.mouse.sum()">Click This Big Sum Button To Total X + Y</button>
+</template>
